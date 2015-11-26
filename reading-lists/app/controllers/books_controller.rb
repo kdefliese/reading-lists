0c87836b30a4f28b555c356a448d84b8845284e0
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-25 21:54:24 -0800

    added book controller methods

--- reading-lists/app/controllers/books_controller.rb
+++ reading-lists/app/controllers/books_controller.rb
@@ -1,2 +1,48 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params[:book])
+    redirect_to "/"
+  end
+
+  def edit
+    id = params[:id]
+    @book = Book.find(id)
+  end
+
+  def show
+    id = params[:id]
+    @book = Book.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @book = Book.find(id)
+    @book.update(
+      title: book_params[:book][:title],
+      author: book_params[:book][:author],
+      read_status: book_params[:book][:read_status],
+      genre: book_params[:book][:genre]
+      )
+    redirect_to "/"
+  end
+
+  def destroy
+    id = params[:id]
+    Book.destroy(id)
+    redirect_to "/"
+  end
+
+  private
+
+  def book_params
+    params.permit(book:[:name])
+  end
 end
