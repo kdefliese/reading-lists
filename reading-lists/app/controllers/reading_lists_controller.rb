a2af31ae22cbc2fd012dbd8a279c27bf331595c7
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-11-25 21:46:50 -0800

    added root route and reading_list controller methods

--- reading-lists/app/controllers/reading_lists_controller.rb
+++ reading-lists/app/controllers/reading_lists_controller.rb
@@ -1,2 +1,46 @@
 class ReadingListsController < ApplicationController
+  def index
+    @reading_lists = ReadingList.all
+  end
+
+  def new
+    @reading_list = ReadingList.new
+  end
+
+  def create
+    ReadingList.create(reading_list_params[:reading_list])
+    redirect_to "/"
+  end
+
+  def edit
+    id = params[:id]
+    @reading_list = ReadingList.find(id)
+  end
+
+  def show
+    id = params[:id]
+    @reading_list = ReadingList.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @reading_list = ReadingList.find(id)
+    @reading_list.update(
+      name: reading_list_params[:reading_list][:name]
+      )
+    redirect_to "/"
+  end
+
+  def destroy
+    id = params[:id]
+    ReadingList.destroy(id)
+    redirect_to "/"
+  end
+
+  private
+
+  def reading_list_params
+    params.permit(reading_list:[:name])
+  end
+
 end
--- reading-lists/config/routes.rb
+++ reading-lists/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'reading_lists#index'
   resources :reading_lists
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
