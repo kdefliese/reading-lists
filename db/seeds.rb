4746bd40cd53cf2f2d589f2596f413329027bc27
Dreedle <davis.audrey.lynn@gmail.com>
2015-12-25 16:33:07 -0800

    Adding review seeds (preliminary ones)

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,27 +49,27 @@ seed_books.each do |book|
   index +=1
 end
 
-# seed_reviews = [
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-#   {book_id:, body: "", reviewer:""},
-# ]
-#
-# seed_reviews.each do |review|
-#   Review.create(review)
-# end
+seed_reviews = [
+  {book_id: 1, body: "A fun romp through the pseudo-middle ages. Not to be missed.", reviewer:"Jamie C."},
+  {book_id: 1, body: "Winter is coming! Brrrr. The books are way better than the TV series.", reviewer:"Alexandra P."},
+  {book_id: 2, body: "Whose eye? Why, the eye of the world, of course! Sit back and enjoy the ride. It will make you laugh. It will make you weep. It will make you think about your humanity and the humanity of those around you.", reviewer:"John H.P."},
+  {book_id: 3, body: "f", reviewer:"Marigold Weiss"},
+  {book_id: 4, body: "f", reviewer:"Samantha P."},
+  {book_id: 4, body: "f", reviewer:"Mia M."},
+  {book_id: 5, body: "f", reviewer:"Hamworth G. Wellington"},
+  {book_id: 6, body: "f", reviewer:"Wixby Wick"},
+  {book_id: 6, body: "f", reviewer:"X. Lao"},
+  {book_id: 7, body: "f", reviewer:"Eric Danforth"},
+  {book_id: 8, body: "f", reviewer:"Jesus G."},
+  {book_id: 9, body: "f", reviewer:"Remmington Wadsworth"},
+  {book_id: 9, body: "f", reviewer:"Maximillian W.D."},
+  {book_id: 9, body: "f", reviewer:"Koji Nakamura"},
+  {book_id: 10, body: "f", reviewer:"Toby Stubbins"},
+  {book_id: 11, body: "f", reviewer:"Hannah P.G."},
+  {book_id: 13, body: "f", reviewer:"P.J. Merriweather"},
+  {book_id: 13, body: "f", reviewer:"M.J. Madden"}
+]
+
+seed_reviews.each do |review|
+  Review.create(review)
+end
