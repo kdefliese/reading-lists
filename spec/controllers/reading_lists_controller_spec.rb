84ea3a1d8c12055ab648743c967bd68e6e162efe
Amy Hunter <amyrhunter@gmail.com>
2015-12-30 14:15:31 -0800

    Install gems, make tests for index, new and create in reading lists controller.

--- spec/controllers/reading_lists_controller_spec.rb
+++ spec/controllers/reading_lists_controller_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ReadingListsController, type: :controller do
+
+  describe "GET 'index'" do
+    it "renders the index view" do
+      get :index
+      expect(subject).to render_template :index
+    end
+  end
+
+  describe "GET 'new'" do
+    it "renders the template for a new reading list" do
+      get :new
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "POST 'create'" do
+    it "creates a new reading list" do
+      last_list = ReadingList.last
+      post :create
+      expect(ReadingList.last).to_not eq last_list
+    end
+  end
 end
